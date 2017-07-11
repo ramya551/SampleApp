934468a9fd8b89027d02f349370d7cfccc711561
Ramya <ramyasrimuthineni12@gmail.com>
2017-07-11 00:23:50 -0500

    Sample App commit2 Added basic form in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductComponent } from './product/product.component';
 import { InventoryComponent } from './inventory/inventory.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
 const routes: Routes = [
 
 	{ path: 'product', component: ProductComponent },
-	{ path: 'inventory', component: InventoryComponent }
+	{ path: 'inventory', component: InventoryComponent },
+	{ path: 'productdetails', component: ProductFormComponent}
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
